fa26d2b6e9e4d13707c29981718bcb19ec0d9ca3
Charles Craven-Bartle <ccravenbartle@gmail.com>
2022-04-15 16:12:13 +0100

    220415 1611

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,67 +8,67 @@ const config = require("../config");
 // of a request
 // It will verify access token against Auth0 JSON web key set
 exports.checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 10,
-    jwksUri: "https://lingoklik.eu.auth0.com/.well-known/jwks.json",
-  }),
-  audience: "https://lingoklik.eu.auth0.com/api/v2/",
-  issuer: "https://lingoklik.eu.auth0.com/",
-  algorithms: ["RS256"],
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 10,
+		jwksUri: "https://portfolios.eu.auth0.com/.well-known/jwks.json",
+	}),
+	audience: "https://portfolios.eu.auth0.com/api/v2/",
+	issuer: "https://portfolios.eu.auth0.com/",
+	algorithms: ["RS256"],
 });
 
 exports.checkRole = (role) => (req, res, next) => {
-  const user = req.user;
+	const user = req.user;
 
-  if (user && user[config.AUTH0_NAMESPACE + "/roles"].includes(role)) {
-    next();
-  } else {
-    return res
-      .status(401)
-      .send("You are not authorized to access this resource!");
-  }
+	if (user && user[config.AUTH0_NAMESPACE + "/roles"].includes(role)) {
+		next();
+	} else {
+		return res
+			.status(401)
+			.send("You are not authorized to access this resource!");
+	}
 };
 
 exports.getAccessToken = (callback) => {
-  const options = {
-    method: "POST",
-    url: config.AUTH0_TOKEN_URL,
-    headers: { "content-type": "application/json" },
-    form: {
-      grant_type: "client_credentials",
-      client_id: config.AUTH0_CLIENT_ID,
-      client_secret: config.AUTH0_CLIENT_SECRET,
-      audience: config.AUTH0_AUDIENCE,
-    },
-  };
+	const options = {
+		method: "POST",
+		url: config.AUTH0_TOKEN_URL,
+		headers: { "content-type": "application/json" },
+		form: {
+			grant_type: "client_credentials",
+			client_id: config.AUTH0_CLIENT_ID,
+			client_secret: config.AUTH0_CLIENT_SECRET,
+			audience: config.AUTH0_AUDIENCE,
+		},
+	};
 
-  return new Promise((resolve, reject) => {
-    request(options, (error, res, body) => {
-      if (error) {
-        return reject(new Error(error));
-      }
+	return new Promise((resolve, reject) => {
+		request(options, (error, res, body) => {
+			if (error) {
+				return reject(new Error(error));
+			}
 
-      resolve(body ? JSON.parse(body) : "");
-    });
-  });
+			resolve(body ? JSON.parse(body) : "");
+		});
+	});
 };
 
 exports.getAuth0User = (accessToken) => (userId) => {
-  const options = {
-    method: "GET",
-    url: `${config.AUTH0_DOMAIN}/api/v2/users/${userId}?fields=name,picture,user_id`,
-    headers: { authorization: `Bearer ${accessToken}` },
-  };
+	const options = {
+		method: "GET",
+		url: `${config.AUTH0_DOMAIN}/api/v2/users/${userId}?fields=name,picture,user_id`,
+		headers: { authorization: `Bearer ${accessToken}` },
+	};
 
-  return new Promise((resolve, reject) => {
-    request(options, (error, res, body) => {
-      if (error) {
-        return reject(new Error(error));
-      }
+	return new Promise((resolve, reject) => {
+		request(options, (error, res, body) => {
+			if (error) {
+				return reject(new Error(error));
+			}
 
-      resolve(body ? JSON.parse(body) : "");
-    });
-  });
+			resolve(body ? JSON.parse(body) : "");
+		});
+	});
 };
